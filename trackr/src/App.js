fba031255eeaaeeab6977f7f5e57d52b58f76b0e
Jason Pham <70985186+phamja1@users.noreply.github.com>
2022-11-02 14:39:53 -0400

    Remove boiler

--- trackr/src/App.js
+++ trackr/src/App.js
@@ -27,10 +27,7 @@ function App() {
           <a className="close" onClick={closeModal}>
             &times;
           </a>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae magni
-          omnis delectus nemo, maxime molestiae dolorem numquam mollitia, voluptate
-          ea, accusamus excepturi deleniti ratione sapiente! Laudantium, aperiam
-          doloribus. Odit, aut.
+          L
         </div>
       </Popup>
       
