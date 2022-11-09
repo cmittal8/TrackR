a1ad4922451c70de368a5988554a4ee55f3debe9
Jason Pham <70985186+phamja1@users.noreply.github.com>
2022-11-09 13:56:35 -0500

    Add Google OAuth

--- trackr/src/App.js
+++ trackr/src/App.js
@@ -6,27 +6,28 @@ import { useState } from 'react';
 import 'reactjs-popup/dist/index.css';
 import Popup from 'reactjs-popup'
 import moment from 'moment';
+import { GoogleLogin } from '@react-oauth/google';
+
+var apiKey = "AIzaSyBPJmfyTPfRPGV566hxysCCkv3H8TscVJQ"
+var clientId = "739140650399-hdlcrsphdb1eh83tgh95q5e9bop0nck6.apps.googleusercontent.com"
+var discoveryDocs = ["https://sheets.googleapis.com/$discovery/rest?version=v4"]
+var scopes = [
+  "https://www.googleapis.com/auth/spreadsheets",
+  "profile"
+]
+
+
+
 
 
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
   const [open, setOpen] = useState(false);
   const closeModal = () => setOpen(false);
 
-  const [companyName, setCompanyName] = useState('');
-  const changeCompanyName = event => setCompanyName(event.target.value);
-
-  const [jobTitle, setJobTitle] = useState('');
-  const changeJobTitle = event => setJobTitle(event.target.value);
-
-  const [startDate, setStartDate] = useState('');
-  const changeStartDate = event => setStartDate(event.target.value);
-
-  let currDate = moment().format("MM/DD/YYYY");
-  const [dateApplied, setDateApplied] = useState(currDate);
-  const changeDateApplied = event => setDateApplied(event.target.value);
-
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -40,8 +41,54 @@ function App() {
           <a className="close" onClick={closeModal}>
             &times;
           </a>
+          <br></br>
+          {
+            !loggedIn?
+            <GoogleLogin
+              onSuccess={credentialResponse => {
+                console.log(credentialResponse)
+                setLoggedIn(true)
+              }}
+              onError={() => {
+                console.log('Login Failed');
+              }}
+              useOneTap
+            /> : <Forms/>
+          }
+        </div>
+      </Popup>
+      
+        <br/><br/>
+      
+          <a
+          className="App-link"
+          href="https://docs.google.com/spreadsheets/d/1ITTlbL4_oWT3mWk4XAuFpX-sN8uwTRQWrRnNFchPJ00/edit#gid=0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go To Spreadsheet 
+        </a>
+        </header>
+    </div>
+  );
+}
 
-          Company Name:
+function Forms () {
+  const [companyName, setCompanyName] = useState('');
+  const changeCompanyName = event => setCompanyName(event.target.value);
+
+  const [jobTitle, setJobTitle] = useState('');
+  const changeJobTitle = event => setJobTitle(event.target.value);
+
+  const [startDate, setStartDate] = useState('');
+  const changeStartDate = event => setStartDate(event.target.value);
+
+  let currDate = moment().format("MM/DD/YYYY");
+  const [dateApplied, setDateApplied] = useState(currDate);
+  const changeDateApplied = event => setDateApplied(event.target.value);
+  return (
+  <>
+    Company Name:
           <input
             type={"text"}
             id="companyName"
@@ -77,30 +124,15 @@ function App() {
             onChange={changeDateApplied}
             value={dateApplied}
           />
+          
 
           <br></br>
 
           <button>
             Submit
           </button>
-        </div>
-      </Popup>
-      
-        <br/><br/>
-      
-          <a
-          className="App-link"
-          href="https://docs.google.com/spreadsheets/d/1ITTlbL4_oWT3mWk4XAuFpX-sN8uwTRQWrRnNFchPJ00/edit#gid=0"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go To Spreadsheet 
-        </a>
-        </header>
-    </div>
+  </>
   );
 }
-
-
 
 export default App;
--- trackr/src/index.tsx
+++ trackr/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="739140650399-hdlcrsphdb1eh83tgh95q5e9bop0nck6.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
